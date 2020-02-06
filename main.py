d3f28e143cc24e695b23e57142664a8c9baee651
av-2599 <43505807+av-2599@users.noreply.github.com>
2020-02-05 23:59:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -102,14 +102,17 @@ def communicate():
         if inp.lower() == "quit":
             break
         
-        results = model.predict([bag_of_words(inp, words)])
+        results = model.predict([bag_of_words(inp, words)])[0]
         results_index = numpy.argmax(results)
         tag = labels[results_index]
-
-        for tg in data["intents"]:
-            if tg["tag"] == tag:
-                responses = tg["responses"]
-        print(random.choice(responses))
+        
+        if results[results_index] > 0.7:
+            for tg in data["intents"]:
+                if tg["tag"] == tag:
+                    responses = tg["responses"]
+            print(random.choice(responses))
+        else:
+            print("I am not trained to answer that. Sorry!")
 
 if __name__ == "__main__":
     communicate()
