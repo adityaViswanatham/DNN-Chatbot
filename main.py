a11cacd03cca4dcc0b7f2d8a6cc430faa5c6c733
av-2599 <43505807+av-2599@users.noreply.github.com>
2020-02-05 23:38:48 -0600

    main driver program

---
+++ main.py
@@ -0,0 +1,115 @@
+# Imports
+import numpy
+import tflearn
+import json
+import tensorflow
+import random
+import pickle
+
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    # Pre-processing data.
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+    labels = sorted(labels)
+
+    training = []
+    output = []
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
+
+    # Changing list form to arrays to train model.
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    # Saving data into a pickle file to prevent redundant pre-processing.
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+# Model for the chatbot.
+# Model works on the basis of probability.
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load("tflearn.model")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("tflearn.model")
+
+# Function to return a bag of words for user provided sentences.
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    # Loop to mark words in the bag.
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+# Function to enable user-bot communication.
+def communicate():
+    print("Start talking to the bot.(Type 'quit' to stop)")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+        
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                responses = tg["responses"]
+        print(random.choice(responses))
+
+if __name__ == "__main__":
+    communicate()
